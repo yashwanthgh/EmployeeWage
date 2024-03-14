e089f7ea1f432d05ab4f36cf61b2c294545591ae
yashwanthgh <yugnasura@gmail.com>
2024-03-14 16:41:50 +0530

    Added: class that calculates monthly wage

---
+++ EmployeeWage/EmployeeWage/MonthlyWage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class MonthlyWage
+    {
+        private int totalDays;
+        private double dailyWage;
+
+        public MonthlyWage(double dailyWage,int totalDays )
+        {
+            this.totalDays = totalDays;
+            this.dailyWage = dailyWage;
+        }
+
+        public double totalWage()
+        {
+            if(totalDays >= 20)
+            {
+                return totalDays * dailyWage;
+            } else
+            {
+                return 0.0;
+            }
+            
+        }
+    }
+}
+                                                                                                                                                            
